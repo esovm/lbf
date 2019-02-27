6826bf1444f9f9c2c06c7daa4ef17c927334c82c
Παλαιολόγος <marekszuwarek1958@gmail.com>
2019-02-27 17:12:47 +0100

    Update lbf.c

--- lbf.c
+++ lbf.c
@@ -1,4 +1,22 @@
 
+/* lbf
+ * Copyright (C) Kamila Palaiologos Szewczyk, 2019.
+ * License: MIT
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy,
+ * modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the
+ * Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+ * WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -107,7 +125,7 @@ process(blk) pblock_t blk; {
                     case '*':
                         amount = blockid(code + i + 1, NULL);
                         if (amount == -1) {
-                            fprintf(stderr, "Unresolver external: %s:%d in %s\n", code + i + 1, i + 1, blk->label);
+                            fprintf(stderr, "Unresolved external: %s:%d in %s\n", code + i + 1, i + 1, blk->label);
                             abort();
                         }
                         
